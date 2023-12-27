efd7374a1171d21736a93ab31143b421bd45250c
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2023-12-27 13:54:22 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Archytas.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Archytas-user \
+    omni_Archytas-userdebug \
+    omni_Archytas-eng
